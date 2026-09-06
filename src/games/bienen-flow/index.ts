1d5f180147024e5aa08bd1968cfc010df95d748a
Thomas Elsen <thomaselsen84@gmail.com>
2026-09-06 23:06:50 +0200

    feat(bienen-flow): export tapTrayDetailed + ClearWave

--- src/games/bienen-flow/index.ts
+++ src/games/bienen-flow/index.ts
@@ -3,10 +3,12 @@ export { createBienenLevel } from './level'
 export {
   createMatch,
   tapTray,
+  tapTrayDetailed,
   remainingCells,
   reachableColors,
   isWon,
   isLost,
 } from './engine'
+export type { ClearWave, TapResult } from './engine'
 export type { BienenLevel, BienenState, BienenPhase, CellColor } from './types'
 export { BIENEN_MAX_LEVEL, COLOR_HEX } from './types'
